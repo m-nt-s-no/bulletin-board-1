abb706b8a5e352ed860060bdc972cff881263792
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-12-15 20:14:20 +0000

    added active_posts, expired_posts to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,13 @@
 #  board_id   :integer
 #
 class Post < ApplicationRecord
+  def active_posts
+    all_active_posts = Post.where(:expires_on > Time.current)
+    return all_active_posts
+  end
+
+  def expired_posts
+    all_expired_posts = Post.where(:expires_on < Time.current)
+    return all_expired_posts
+  end
 end
